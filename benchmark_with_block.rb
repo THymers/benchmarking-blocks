27957e5707e4b3d7de8eab7e600aa6af84bafff6
Tara <hymerstara@gmail.com>
2024-06-27 23:25:49 -0600

    "Implement the benchmark method"

--- benchmark_with_block.rb
+++ benchmark_with_block.rb
@@ -1,11 +1,16 @@
 def benchmark
-  # Your benchmarking code goes here.
+  start_time = Time.now # Record the start time
+  yield # Execute the block
+  end_time = Time.now # Record the end time
+  elapsed_time = end_time - start_time # Calculate the elapsed time
+  elapsed_time # Return the elapsed time
 end
- 
-# Be careful, pasting this into IRB will take a long time to print.
+
+# Example usage
+# Be careful, pasting this into IRB/Pry will take a long time to print.
 # It's a loooong string. :)
-long_string = "apple"*100000000
- 
+long_string = "apple" * 100000000
+
 running_time = benchmark { long_string.reverse }
- 
+
 puts "string.reverse took #{running_time} seconds to run"
